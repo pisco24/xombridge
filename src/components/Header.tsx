57b170bb3f71377b729ed610f8edd4b3cc3df2a2
pisco24 <boydakar101@gmail.com>
2025-03-20 18:20:56 -0400

    Force dynamic rendering and adjust app directory for dynamic backgroundssssbiuguhvvj

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -35,7 +35,7 @@ const Header = () => {
   }, []);
 
   return (
-    <Box sx={{ zIndex: "3", top: "0", left: "0", py: "16px", px: "16px", width: "100%" }}>
+    <Box sx={{ zIndex: 3, top: 0, left: 0, py: "16px", px: "16px", width: "100%" }}>
       <Box
         sx={{
           display: "flex",
@@ -46,39 +46,32 @@ const Header = () => {
         }}
       >
         {/* Left Section: Logo & Banner */}
-        <Box
-          sx={{
-            display: "flex",
-            gap: "0.5rem",
-            alignItems: "center",
-            height: "3rem",
-          }}
-        >
+        <Box sx={{ display: "flex", gap: "0.5rem", alignItems: "center", height: "3rem" }}>
           <a href="https://xombol.io" target="_blank" rel="noopener noreferrer">
             <Typography
-              component={"img"}
+              component="img"
               src={logo_mobile.src}
               className="xombol-logo"
               sx={{
                 cursor: "pointer",
-                width: "140px", // 40% bigger than assumed 100px
+                width: "140px", // 40% bigger than before
                 height: "auto",
               }}
             />
           </a>
           <Typography
-            component={"img"}
+            component="img"
             src={banner.src}
             className="xombol-banner"
             sx={{
               cursor: "pointer",
-              width: "140px", // 40% bigger than assumed 100px
+              width: "140px", // 40% bigger than before
               height: "auto",
             }}
           />
         </Box>
 
-        {/* Center Section: Social Bar */}
+        {/* Center Section: Social Tab */}
         <Box
           sx={{
             display: "flex",
@@ -101,7 +94,7 @@ const Header = () => {
               height="24"
               viewBox="0 0 24 24"
             >
-              <path d="M19.633 7.997c.013.177.013.353.013.53 0 5.392-4.1 11.61-11.61 11.61-2.3 0-4.436-.67-6.234-1.82.321.038.643.05.967.05 1.895 0 3.64-.643 5.033-1.722a4.063 4.063 0 0 1-3.793-2.82c.25.038.5.063.75.063.375 0 .75-.05 1.1-.138a4.062 4.062 0 0 1-3.253-3.98v-.05c.55.3 1.188.477 1.87.5a4.062 4.062 0 0 1-1.812-3.385c0-.75.2-1.45.55-2.05a11.535 11.535 0 0 0 8.375 4.25 4.57 4.57 0 0 1-.1-.93 4.062 4.062 0 0 1 4.062-4.062c1.17 0 2.225.5 2.97 1.3a8.007 8.007 0 0 0 2.57-.98 4.062 4.062 0 0 1-1.78 2.23 8.15 8.15 0 0 0 2.35-.64 8.74 8.74 0 0 1-2.03 2.1z" />
+              <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
             </svg>
           </a>
           {/* Telegram Icon */}
@@ -116,9 +109,9 @@ const Header = () => {
               fill="currentColor"
               width="24"
               height="24"
-              viewBox="0 0 24 24"
+              viewBox="0 0 240 240"
             >
-              <path d="M12 0C5.37258 0 0 5.37258 0 12C0 18.6274 5.37258 24 12 24C18.6274 24 24 18.6274 24 12C24 5.37258 18.6274 0 12 0ZM17.5938 8.20312L15.5859 16.1719C15.4336 16.8516 15.1172 16.999 14.5664 16.7812L11.2266 14.2109L9.5625 15.8047C9.38281 15.9844 9.19531 16.1719 8.97656 16.1719L8.95312 13.7812L15.3281 8.94531C15.6641 8.69531 15.3125 8.61719 15.0625 8.86719L8.19531 14.1016L5.42188 13.5C4.75781 13.3359 4.74219 12.7656 5.35938 12.5703L16.0234 9.17969C16.5234 9.03125 17.0156 9.35938 16.9531 9.84375C16.8828 10.2578 16.5742 10.4922 16.2109 10.4375L9.50781 9.61719L8.89844 7.10156L16.1094 7.82812C16.8125 7.92969 17.5234 8.11719 17.5938 8.20312Z" />
+              <path d="M120 0C53.73 0 0 53.73 0 120s53.73 120 120 120 120-53.73 120-120S186.27 0 120 0zm58.08 83.93L158.4 176.52c-1.49 6.61-5.38 8.25-10.88 5.15l-30.08-22.2-14.5 14c-1.6 1.6-2.93 2.93-6 2.93l2.14-30.33L94.91 114.3c-2.4-2.14.52-3.33 3.74-1.19l55.33 50.1 15.13-13.95c6.87-6.37 2.64-9.93-4.12-3.87l-53.87 43.92L85.12 126.9l93.96-73.97c4.34-3.4 8.27-1.62 6.08 2.9z" />
             </svg>
           </a>
         </Box>
@@ -144,7 +137,7 @@ const Header = () => {
             </Box>
           ) : (
             <Box className="btn_wrap_connect">
-              <ConnectButton chainStatus={"none"} label={"Select Wallet"} />
+              <ConnectButton chainStatus="none" label="Select Wallet" />
             </Box>
           )}
         </Box>
